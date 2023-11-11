b2a082391808a7e25fb3e7e1c55e568cd8a5da8c
Ajay Prasad Sarode <sarodeajayprasad@gmail.com>
2023-11-11 16:42:22 +0530

    drop by drop, the bucket gets filled

---
+++ 057-hands-on-exercise-14/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/sarodeajayprasad/puppy"
+)
+
+func main() {
+	fmt.Println("Hello Gophers💖")
+	fmt.Println(puppy.Bark())
+	fmt.Println(puppy.Barks())
+	fmt.Println(puppy.BigBark())
+	fmt.Println(puppy.BigBarks())
+	puppy.From13()
+
+}
